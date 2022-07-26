bb3f18d8756ddaa2afb020346ae50b206d78bfc3
pnlsravani <94427494+pnlsravani@users.noreply.github.com>
2022-07-26 20:28:09 +0530

    Add files via upload

---
+++ manifests/cisco.js
@@ -0,0 +1,16 @@
+
+
+const http = require('http');
+
+http.createServer((request, response) => {
+
+   
+        'Content-Type': 'text/plain'
+    });
+
+   
+    response.write('CWiS Assessment\n');
+
+   
+
+}).listen(6000); // 
